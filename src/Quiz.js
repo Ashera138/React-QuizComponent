0d6188235af2bce978afd44e37e7455f55f4edf3
Lauren Van Sloun <vanslounl@yahoo.com>
2019-01-23 14:50:49 -0500

    Module 7 - Code for Resetting the Quiz - End of course

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,21 +5,26 @@ import QuizEnd from './QuizEnd.js';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-    showNextQuestion() {
-        this.setState((state) => {
-            return { quiz_position: state.quiz_position + 1 }
-        })
-    };
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1 };
-    }
+    };
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 };
+        });
+    };
+    handleResetClick() {
+        this.setState((state) => {
+            return { quiz_position: 1 };
+        });
+    };
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         return (
         <div>
             {isQuizEnd ? 
-                <QuizEnd /> :
+                <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}
                     showNextQuestionHandler={this.showNextQuestion.bind(this)} />
             }
